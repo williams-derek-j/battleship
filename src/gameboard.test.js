3b4faaec667085d178cd7ef26d3b03b287c9634a
williams-derek-j <williams-derek-j@proton.me>
2025-09-20 02:31:00 -0700

    init commit

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -36,8 +36,8 @@ test('Hit boat', () => {
     board.events.on('hit', listener)
 
     expect(board.place([9,17,25,33,41,49])).toBeTruthy()
-    expect(board.hit(9)).toBeTruthy()
+    expect(board.hit(9)).toBeTruthy() // Probably the board should take as little information as possible. Each player can listen and check if they possess a boat in the position of the successful hit, which I don't think should be too costly, even for both players each hit.
 
     expect(listener).toHaveBeenCalled()
-    expect(listener).toHaveBeenCalledWith(expect.objectContaining({ pos: 9 })) // The listener represents either a player object or boat object that listeners for the board to emit hit events
+    expect(listener).toHaveBeenCalledWith(expect.objectContaining({ pos: 9, turn: 0, dead: false, gameFinished: false })) // The listener represents either a player object or boat object that listeners for the board to emit hit events
 })
