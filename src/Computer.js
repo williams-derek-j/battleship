d8e19afb41824fed04f12910c3358d17f0d28e79
williams-derek-j <williams-derek-j@proton.me>
2025-10-10 18:30:58 -0700

    more attack generation refactoring

--- src/Computer.js
+++ src/Computer.js
@@ -190,7 +190,7 @@ export default class Computer extends Player {
 
                 let right = hit + 1
                 let endR = right
-                while (available.includes(endR) && endR <= mod + boardLength) {
+                while (available.includes(endR) && endR < mod + boardLength) {
                     endR += 1
                 }
 
@@ -233,94 +233,61 @@ export default class Computer extends Player {
             } else {
                throw Error('chain of hits somehow had more than 1 valid adjacent target assigned, something is wrong w/ logic')
             }
-        } else { // no hits, pick random square or most isolated square (exploitable if not unpredictably switching between most-isolated and true random targets)
+        } else { // no standing hits on board, only empty squares or sunken ships, so pick random square or most isolated square (exploitable if not unpredictably switching between most-isolated and true random targets)
             function randomInt(min, max) {
                 return Math.floor(Math.random() * (max - min) ) + min;
             }
 
-            const valid = [] // squares adjacent to generated target who indicate that a ship fits in that direction
+            let valid = false // squares adjacent to generated target who indicate that a ship fits in that direction
 
-            while (valid.length === 0) {
+            while (valid === false) {
                 const index = randomInt(0, available.length)
                 const target = available[index]
-                console.log('generated random target:', target)
+                // console.log('generated random target:', target, 'available', available)
 
-                // let mod = (target - (target % boardLength)) + boardLength
+                let mod = target - (target % boardLength)
+                // console.log('mod',mod)
 
-                // let left = target - 1) >= (mod - boardLength) ? target - 1 : target // prevent wrapping
                 let left = target - 1
-                // let right = (target + 1) < mod ? target + 1 : target
+                let endL = left
+                while (available.includes(endL) && endL >= mod) {
+                    endL -= 1
+                }
+                // console.log('l,endL',left,endL)
+
                 let right = target + 1
-                let top = target - boardLength
-                let bottom = target + boardLength
+                let endR = right
+                while (available.includes(endR) && endR < mod + boardLength) {
+                    endR += 1
+                }
+                // console.log('r,endR',right,endR)
 
-                while (available.includes(left) && left !== target) { // LEFT - how much free space to left? prefer left, then right
-                    left -= 1
+                if ((endR - endL) - 1 >= shipMin) {
+                    if (available.includes(target)) {
+                        valid = true
+                    }
+                } else {
+                    let top = target - boardLength
+                    let endT = top
+                    while (available.includes(endT) && endT >= 0) {
+                        endT -= boardLength
+                    }
 
-                    if (!available.includes(left)) { // found end of free space on left
-                        if (!available.includes(right)) { // found end of free space on right -- there was none
-                            if ((right - left) - 1 >= shipMin) {
-                                valid.push(target - 1) // no free space on right but enough on left to fit smallest ship, therefore 1 square left of hit is valid target
-                            }
-                        } else {
-                            while (available.includes(right)) { // find free space on right
-                                right += 1
+                    let bottom = target + boardLength
+                    let endB = bottom
+                    while (available.includes(endB) && endB <= (boardLength ** 2)) {
+                        endB += boardLength
+                    }
 
-                                if (!available.includes(right)) { // found end of free space on both sides
-                                    if ((right - left) - 1 >= shipMin) { // amount of space can fit smallest ship? -- right and left are unavailable spaces, so don't + 1,  actually - 1
-                                        valid.push(target - 1) // if so, square is valid target
-                                    }
-                                }
-                            }
+                    if (((endB - endT) / boardLength) - 1 >= shipMin) {
+                        if (available.includes(target)) {
+                            valid = true
                         }
                     }
                 }
-                if (valid.length === 0) { // RIGHT - preferred left but was none, now check right -- can only get here if there was no valid target to left
-                    while (available.includes(right) && right !== target) {
-                        right += 1
-                    }
-                    if ((right - left) - 1 >= shipMin) { // bottom and top are both unavailable, so count squares between
-                        if (available.includes(target + 1)) {
-                            valid.push(target + 1)
-                        }
-                    }
-                    if (valid.length === 0) { // TOP - prefer left, then right, then top -- can only get here if there was no valid target to left or right
-                        while (available.includes(top)) {
-                            top -= boardLength
-
-                            if (!available.includes(top)) { // found end of free space on top
-                                if (!available.includes(bottom)) { // found end of free space on bottom -- there was none
-                                    if (((bottom - top) / boardLength) - 1 >= shipMin) {
-                                        valid.push(target - boardLength) // no free space on right but enough on left to fit smallest ship, therefore 1 square above hit is valid target
-                                    }
-                                } else {
-                                    while (available.includes(bottom)) { // find free space on bottom
-                                        bottom += boardLength
-
-                                        if (!available.includes(bottom)) { // found end of free space on both sides
-                                            if (((bottom - top) / boardLength) - 1 >= shipMin) { // amount of space can fit smallest ship? -- top and bottom are unavailable spaces, so don't + 1,  actually - 1
-                                                valid.push(target -  boardLength) // if so, square is valid target
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        if (valid.length === 0) { // BOTTOM -- prefer left, then right, then top, finally bottom if none else
-                            while (available.includes(bottom)) {
-                                bottom += boardLength
-                            }
-
-                            if (((bottom - top) / boardLength) - 1 >= shipMin) { // bottom and top are both unavailable, so count squares between
-                                if (available.includes(target + boardLength)) {
-                                    valid.push(target + boardLength)
-                                }
-                            }
-                        }
-                    }
-                }
-                if (valid.length === 1) {
+                if (valid) {
                     console.log('C generated target:')
+
                     this.attack(target) // isolated hit square had valid targets adjacent
                 } else {
                     available.splice(index, 1)
@@ -328,11 +295,12 @@ export default class Computer extends Player {
                     if (available.length === 0) {
                         throw Error("Computer couldn't find valid target to generate, all available spaces invalid.")
                     } else {
+                        this.board.offense[target] = -2 // mark as an invalid square so we don't have to recheck later
+
                         console.log("Computer generated target didn't have space to fit smallest ship! Trying again with new target.")
                     }
                 }
             }
-            // write logic to find most isolated square using adjacency list, target being the node w/ most free adjacents
         }
     }
 
